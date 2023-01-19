fe27ee9e4f8efe19354a2128ae743fa954f16208
yljh1 <57140266+yljh1@users.noreply.github.com>
2023-01-19 08:15:15 +0100

    Update navigation.component.ts

--- src/app/authorized/navigation/navigation.component.ts
+++ src/app/authorized/navigation/navigation.component.ts
@@ -1,19 +1,50 @@
+<<<<<<< Updated upstream
 import { Component, OnInit } from '@angular/core';
+<<<<<<< Updated upstream
 import {SupabaseService} from "../../services/supabase.service";
+=======
+=======
+import {Component, OnInit} from '@angular/core';
+import {SupabaseService} from "../../services/supabase.service";
+import {NotificationService} from "../../services/notification.service";
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+	selector: 'app-navigation',
+	templateUrl: './navigation.component.html',
+	styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
 
+<<<<<<< Updated upstream
   constructor(
 	  private supabaseService: SupabaseService
   ) { }
 
 	public signOut(){
 		this.supabaseService.signOut()
+=======
+<<<<<<< Updated upstream
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+=======
+	constructor(
+		private supabaseService: SupabaseService,
+		private notifyService: NotificationService
+	) {
+>>>>>>> Stashed changes
 	}
 
+	public signOut() {
+		this.signOutNotification();
+		this.supabaseService.signOut()
+	}
+>>>>>>> Stashed changes
+
+	signOutNotification() {
+		this.notifyService.showInfo("Hope to see you again soon", "See ya!")
+	}
 }
