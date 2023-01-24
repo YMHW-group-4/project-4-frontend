0626d5a89346af83d19583ecd5e9eb977e666212
Merlijn <merlijnjoeve@hotmail.com>
2023-01-24 13:19:55 +0100

    updated the node server

--- node_backend/index.js
+++ node_backend/index.js
@@ -5,19 +5,22 @@ const app = express()
 app.use(cors());
 
 const port = 3000;
-const availableNodes = [];
+var availableNodes = [];
 
 app.get('/register_node', (req, res) => {
 	const port = req.query.port
 	const host = req.query.host
+	const ipString = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+	const ipSplit = ipString.split(':');
+	const ip = ipSplit[ipSplit.length - 1];
 
 	if (port != null && host != null) {
-		const url = `http://${host}:${port}`;
+		const url = `http://${host}${port}`;
 		const alreadyRegistered = availableNodes.find((node) => node.url === url);
 		if (alreadyRegistered == null) {
-			availableNodes.push({host, port, url, connections: 0});
+			availableNodes.push({host, port, url, connections: 0, ip});
 		}
-		const success = {success: `Node: ${host}:${port} is now registered.`};
+		const success = {success: `Node: ${host}:${port} is now registered.`, ip};
 		res.send(JSON.stringify(success));
 	} else {
 		const error = {
@@ -43,6 +46,17 @@ app.get('/get_all_nodes', (req, res) => {
 		res.send(JSON.stringify(availableNodes));
 	} catch (_) {
 		const error = {error: 'No nodes registered, Register a node using /register_node?host=[HOST]&port=[PORT]'}
+		res.send(JSON.stringify(error));
+	}
+})
+
+
+app.get('/clear', (req, res) => {
+	availableNodes = [];
+	try {
+		res.send("All nodes are cleared");
+	} catch (_) {
+		const error = {error: 'Something whened wrong'}
 		res.send(JSON.stringify(error));
 	}
 })
